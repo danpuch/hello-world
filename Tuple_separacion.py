f0f9b5601387a1fb08a916f410ba7bf66b12dd60
Dan <41241440+danpuch@users.noreply.github.com>
2019-06-17 17:41:24 +0000

    Add files via upload
    welldone!"

---
+++ Tuple_separacion.py
@@ -0,0 +1,20 @@
+'''funcion que solicite dia, mes año .
+otra funcion que reciba una tupla con la
+fecha para mostrarla'''
+
+def dataTuple():
+    print("Hello. Please, introduce the following information: ")
+    dd = int(input("Introduce Day: "))
+    mm = input("Introduce Month: ")
+    yy = int(input("Introduce Year: "))
+    return (dd, mm, yy)
+
+def impDate(fecha):
+    print(fecha[0], fecha[1], fecha[2], sep="/")
+
+#bloque principal
+
+fecha = dataTuple()
+impDate(fecha)
+
+
