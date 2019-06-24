a585d0eeebb8df6ea472345a1e135de7e24d0a17
Dan <41241440+danpuch@users.noreply.github.com>
2019-06-24 00:47:09 +0000

    Add files via upload

---
+++ POO_video29.py
@@ -0,0 +1,29 @@
+'''Herencia- video 29 de curso youtube'''
+
+class Vehiculos():
+
+    def __init__(self, marca, modelo):
+        self.marca=marca
+        self.modelo=modelo
+        self.enmarcha=False
+        self.acelera=False
+        self.frena=False
+
+    def arrancar(self):
+        self.enmarcha=True
+
+    def acelerar(self):
+        self.acelera=True
+
+    def frenar(self):
+        self.frena=True
+
+    def estado(self):
+        print("Marca:", self.marca, "\nModelo:", self.modelo, "\nEn marcha:", self.enmarcha
+              ,"\nAcelerando:", self.acelera,"\nFrena:", self.frena)
+
+class Moto(Vehiculos):
+    pass
+
+miMoto=Moto("Honda", "CBR")
+miMoto.estado()
