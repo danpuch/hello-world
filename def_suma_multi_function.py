d49d717c20d169653a667551894976caf7708db4
Dan <41241440+danpuch@users.noreply.github.com>
2019-06-28 21:25:32 +0200

    Add files via upload

---
+++ def_suma_multi_function.py
@@ -0,0 +1,15 @@
+def sumando(listado):
+    suma=0
+    for i in listado:
+        suma+=i
+    print(suma)
+
+def multiplicando(listado):
+    multi=1
+    for i in listado:
+        multi*=i
+    print(multi)
+
+sumando([1,2,3,4])
+multiplicando([1,2,3,4])
+
