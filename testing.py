93b317d285e5dcdb4879a56163003e041fd905c2
Dan <41241440+danpuch@users.noreply.github.com>
2019-06-30 14:55:16 +0200

    Add files via upload

--- testing.py
+++ testing.py
@@ -1,6 +1,30 @@
-#tabla multiplicar
 
-numero=int(input("Numero: "))
 
-for i in range(1,11):
-    print(numero, " por ", i , " igual a ", numero*i)
+def leer_archivo_txt():
+    f = open('texto.txt')
+    print('archivo: ', f.name)
+
+arch = f.read()
+
+crea_lista(arch)
+
+f.close()
+print('cerrado? ', f.closed)
+
+
+def crea_lista(archivo):
+    lista_palabras = []
+    letras = 'qwertyuiopasdfghjklñzxcvbnmQWERTYUIOPASDFGHJKLÑZXCVBNM'
+    palabra = ''
+
+for l in archivo:
+    if l in letras:
+    palabra += l
+elif palabra != '':
+lista_palabras.append(palabra)
+palabra = ''
+
+print(lista_palabras)
+
+
+leer_archivo_txt()
