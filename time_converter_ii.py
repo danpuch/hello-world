f8583166362b12e8137c9b5a9776b094df34468c
Dan <41241440+danpuch@users.noreply.github.com>
2019-07-10 05:19:18 +0200

    Add files via upload

---
+++ time_converter_ii.py
@@ -0,0 +1,20 @@
+def time_converter(time):
+    time1=time.replace(':', ' ').split()
+    hours=int(time1[0])
+    minutes=str(time1[1])
+
+    if hours > 24:
+        return "Error"
+    elif hours == 12:
+        return str(hours) + ':' + str(minutes) + ' p.m.'
+    elif hours == 00:
+        return str(hours +12) + ':' + str(minutes) + ' a.m.'
+    elif 13 <= hours <= 24:
+        return str(hours-12) + ':' + str(minutes) + ' p.m.'
+    else:
+        return str(hours) + ':' + str(minutes) + ' a.m.'
+
+
+if __name__ == '__main__':
+    time = '00:00'
+    print(time_converter(time))
