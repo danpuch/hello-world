f4e8151fd868e3463858ce6f5ec59d8c28b99ad5
Dan <41241440+danpuch@users.noreply.github.com>
2019-07-25 20:36:33 +0200

    Add files via upload

---
+++ interfaces_graficas_i.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+raiz = Tk()
+raiz.title("Ventana de pruebas")
+raiz.resizable(False, False)
+raiz.iconbitmap()
+raiz.geometry("650x350")
+raiz.config(bg="light blue")
+
+raiz.mainloop()
